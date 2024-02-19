927257ce250c48c714089b314a88ccaf864a8dba
SamaelPicoli <76179103+Samaelpicoli@users.noreply.github.com>
2024-02-19 10:44:32 -0300

    Update pega_arquivo.py

--- pega_arquivo.py
+++ pega_arquivo.py
@@ -1,6 +1,7 @@
 import requests
 
 def solicitar_csv():
+    #faz uma requisição ao site e traz o arquivo para minha máquina
     url = 'https://aai-devportal-media.s3.us-west-2.amazonaws.com/challenges/shopping-list.csv'
     r = requests.get(url, allow_redirects=True)
     nome = 'shopping-list.csv'
